12f2376689c6235b93e9408d4e885d1b981825ec
Emmanuel Marcos de Oliveiera <emmanuelmarcosdeoliveira@gmail.com>
2024-08-08 10:52:02 -0300

    chore: Criando o test para o component Produto

---
+++ src/components/Produto/__tests__/Produto.test.tsx
@@ -0,0 +1,28 @@
+import { fireEvent, screen } from '@testing-library/react'
+import Produto from '..'
+import { renderizaComProvider } from '../../../utils/tests'
+
+const jogo = {
+  id: 2,
+  categoria: 'RPG',
+  imagem: '',
+  plataformas: ['Windows', 'PS5', 'Xbox Series S/X'],
+  preco: 199.9,
+  precoAntigo: 299.9,
+  titulo: 'Hogwarts Legacy'
+}
+
+describe('Testes para o componente prodto', () => {
+  test('Deve renderizar corretamente', () => {
+    renderizaComProvider(<Produto game={jogo} />)
+    expect(screen.getByText('Hogwarts Legacy')).toBeInTheDocument()
+  })
+
+  test('Deve Adicionar um item ao Carrinho', () => {
+    const { store } = renderizaComProvider(<Produto game={jogo} />)
+    const botao = screen.getByTestId('btn-adicionar-produto')
+    fireEvent.click(botao)
+    store.getState().carrinho.itens
+    expect(store.getState().carrinho.itens).toHaveLength(1)
+  })
+})
